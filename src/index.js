1e0484a05b5f394defa5d7d3b391f3b5516a14d6
njeri <njeri255mungai@gmail.com>
2024-04-12 09:21:58 +0300

    deploy

---
+++ src/index.js
@@ -0,0 +1,19 @@
+
+const db = "http://localhost:3000/menuItems"
+
+// Function to log a message when the page loads
+window.onload = function () {
+    console.log('Page loaded successfully');
+  };
+  
+  // Function to handle clicking on menu categories
+  function handleMenuClick(category) {
+    console.log('Clicked on menu category:', category);
+    // You can add more logic here based on the category clicked
+  }
+  
+  // Function to handle clicking on order details
+  function handleOrderClick(name, price) {
+    console.log('Clicked on order:', name, '- Price:', price);
+    // You can add more logic here based on the order clicked
+  }
